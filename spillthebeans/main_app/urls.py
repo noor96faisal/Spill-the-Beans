9cf95519066eed609ea19a5fdbda12ad13759ae9
noor96faisal <n.fai2@icloud.com>
2025-06-29 13:40:59 +0300

    update add recipe button

--- spillthebeans/main_app/urls.py
+++ spillthebeans/main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('add/', views.add_recipe, name='add_recipe'), 
     path('login/', auth_views.LoginView.as_view(template_name='main_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'), 
+    path('register/', views.register, name='register'),
+
 
 ]
--- spillthebeans/main_app/views.py
+++ spillthebeans/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import BrewingMethod, Category 
 
 def home(request):
@@ -12,3 +14,23 @@ def home(request):
 
 def add_recipe(request):
     return render(request, 'main_app/add_recipe.html')
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == 'POST':
+        print("POST received")
+        if form.is_valid():
+            print("Form is valid")
+            user = form.save()
+            login(request, user)
+            print("User logged in:", user.username)
+            return redirect('home')
+        else:
+            print("Form errors:", form.errors)
+    else:
+        print("GET request")
+
+    return render(request, 'main_app/register.html', {'form': form})
+
+
