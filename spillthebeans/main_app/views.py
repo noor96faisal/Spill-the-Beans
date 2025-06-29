ce075b8368362e483e4986785ef4f8c02e6a33c9
noor96faisal <n.fai2@icloud.com>
2025-06-29 10:09:51 +0300

    homepage finished

--- spillthebeans/main_app/views.py
+++ spillthebeans/main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import BrewingMethod
 
 # Create your views here.
+def home(request):
+    methods = BrewingMethod.objects.all()
+    return render(request, 'main_app/home.html', {'methods': methods})
+
+def add_recipe(request):
+    return render(request, 'main_app/add_recipe.html')
